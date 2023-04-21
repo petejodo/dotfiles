b018fca967e7a57e22bf4c1bf0d430dc4ff09dbd
Peter Jodogne <pete.jodogne@gmail.com>
2023-04-21 00:06:14 -0400

    enable history in erlang/elixir shell

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -2,11 +2,12 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 
     set -x FLYCTL_INSTALL "~/.fly"
+    set -x ERL_AFLAGS "-kernel shell_history enabled -kernel shell_history_file_bytes 1024000"
     fish_add_path ~/.fly/bin
 
     source ~/.asdf/asdf.fish
     eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-    # eval "$(fnm env)"
+
     fish_add_path ~/.cargo/bin
     fish_add_path ~/.local/bin
     fish_add_path /home/linuxbrew/.linuxbrew/lib/ruby/gems/3.1.0/bin
